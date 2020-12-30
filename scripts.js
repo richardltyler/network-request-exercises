18ba68ce58e45f6a08fca5f6f7291ad6be5b24ea
Richard Tyler <richardtyler2011@gmail.com>
2020-12-29 19:53:26 -0700

    Display gotten users

---
+++ scripts.js
@@ -0,0 +1,24 @@
+displayGottenUsers();
+
+function displayUsers(users) {
+  const displayedUsers = document.querySelector('#users-section');
+  users.forEach(user => {
+    const stringedUser = JSON.stringify(user);
+    displayedUsers.innerHTML += 
+      `<h3>${stringedUser}</h3>`
+  })
+}
+
+function displayGottenUsers() {
+  fetch("http://localhost:3001/api/v1/users")
+  .then(response => response.json())
+  .then(boix => displayUsers(boix))
+}
+
+// function displayUsersAfterPost() {
+//   fetch("http://localhost:3001/api/v1/users", {
+//     method: "POST",
+
+//   })
+// }
+
